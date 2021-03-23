9a18ab68c76f9729db33e5b9b699aecb01c72820
rasoul98 <rasoolmhhmed@gmail.com>
2021-03-23 13:43:53 +0100

    versio-1

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
         fbContent = (
             <div>
                 <HomePage />
-                <div path="/home" className="user_info">
+                <div className="user_info">
                 <img src={this.state.picture} alt={this.state.name} />
                 <h2>Welcome {this.state.name}</h2>
                 Email: {this.state.email}
